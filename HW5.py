a3334161747ee646d50b2a05595c33bea989c593
Katarina Mazanka <katmazan@umich.edu>
2017-10-09 12:24:14 -0400

    First commit

---
+++ HW5.py
@@ -0,0 +1,11 @@
+import re
+filename = 'regex_sum_38687.txt'
+file_object = open(filename, "r")
+text = file_object.read()
+numbers = re.findall(r'[0-9]+', text)
+num_sum = 0
+for item in numbers:
+    item = int(item)
+    num_sum += item
+print(num_sum)
+
